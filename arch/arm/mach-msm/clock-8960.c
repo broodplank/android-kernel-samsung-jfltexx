7733b67f9bc87889ae53bc63bf8f612184ff1e45
Talustus <talustus@dream-irc.de>
2013-05-25 18:51:09 +0200

    Enable GPU Overclock

--- arch/arm/mach-msm/clock-8960.c
+++ arch/arm/mach-msm/clock-8960.c
@@ -3531,6 +3531,10 @@ static struct clk_freq_tbl clk_tbl_gfx3d[] = {
 	F_GFX3D(320000000, pll2,  2,  5),
 	F_GFX3D(400000000, pll2,  1,  2),
 	F_GFX3D(450000000, pll15, 1,  2),
+#ifdef CONFIG_GPU_OVERCLOCK
+	F_GFX3D(533000000, pll15, 1,  2),
+	F_GFX3D(600000000, pll15, 1,  2),	
+#endif
 	F_END
 };
 
@@ -3552,6 +3556,11 @@ static struct clk_freq_tbl clk_tbl_gfx3d_8960[] = {
 	F_GFX3D(300000000, pll3, 1,  4),
 	F_GFX3D(320000000, pll2, 2,  5),
 	F_GFX3D(400000000, pll2, 1,  2),
+#ifdef CONFIG_GPU_OVERCLOCK
+	F_GFX3D(450000000, pll15, 1,  2),
+	F_GFX3D(533000000, pll15, 1,  2),
+	F_GFX3D(600000000, pll15, 1,  2),	
+#endif
 	F_END
 };
 
@@ -3573,38 +3582,64 @@ static struct clk_freq_tbl clk_tbl_gfx3d_8930ab[] = {
 	F_GFX3D(266667000, pll2,  1,  3),
 	F_GFX3D(320000000, pll2,  2,  5),
 	F_GFX3D(400000000, pll2,  1,  2),
+#ifdef CONFIG_GPU_OVERCLOCK
+	F_GFX3D(450000000, pll15, 1,  2),
+	F_GFX3D(533000000, pll15, 1,  2),
+	F_GFX3D(600000000, pll15, 1,  2),	
+#else
 	F_GFX3D(500000000, pll15, 1,  2),
+#endif
 	F_END
 };
 
 static unsigned long fmax_gfx3d_8064ab[VDD_DIG_NUM] = {
 	[VDD_DIG_LOW]     = 128000000,
 	[VDD_DIG_NOMINAL] = 325000000,
+#ifdef CONFIG_GPU_OVERCLOCK
+	[VDD_DIG_HIGH]    = 600000000
+#else
 	[VDD_DIG_HIGH]    = 450000000
+#endif
 };
 
 static unsigned long fmax_gfx3d_8064[VDD_DIG_NUM] = {
 	[VDD_DIG_LOW]     = 128000000,
 	[VDD_DIG_NOMINAL] = 325000000,
+#ifdef CONFIG_GPU_OVERCLOCK
+	[VDD_DIG_HIGH]    = 600000000
+#else
 	[VDD_DIG_HIGH]    = 400000000
+#endif
 };
 
 static unsigned long fmax_gfx3d_8930[VDD_DIG_NUM] = {
 	[VDD_DIG_LOW]     = 192000000,
 	[VDD_DIG_NOMINAL] = 320000000,
+#ifdef CONFIG_GPU_OVERCLOCK
+	[VDD_DIG_HIGH]    = 600000000
+#else
 	[VDD_DIG_HIGH]    = 400000000
+#endif
 };
 
 static unsigned long fmax_gfx3d_8930aa[VDD_DIG_NUM] = {
 	[VDD_DIG_LOW]     = 192000000,
 	[VDD_DIG_NOMINAL] = 320000000,
+#ifdef CONFIG_GPU_OVERCLOCK
+	[VDD_DIG_HIGH]    = 600000000
+#else
 	[VDD_DIG_HIGH]    = 450000000
+#endif
 };
 
 static unsigned long fmax_gfx3d_8930ab[VDD_DIG_NUM] = {
 	[VDD_DIG_LOW]     = 192000000,
 	[VDD_DIG_NOMINAL] = 320000000,
+#ifdef CONFIG_GPU_OVERCLOCK
+	[VDD_DIG_HIGH]    = 600000000
+#else
 	[VDD_DIG_HIGH]    = 500000000
+#endif
 };
 
 static struct bank_masks bmnd_info_gfx3d = {
@@ -3646,7 +3681,11 @@ static struct rcg_clk gfx3d_clk = {
 		.dbg_name = "gfx3d_clk",
 		.ops = &clk_ops_rcg,
 		VDD_DIG_FMAX_MAP3(LOW,  128000000, NOMINAL, 300000000,
+#ifdef CONFIG_GPU_OVERCLOCK
+				  HIGH, 600000000),
+#else
 				  HIGH, 400000000),
+#endif
 		CLK_INIT(gfx3d_clk.c),
 		.depends = &gmem_axi_clk.c,
 	},
